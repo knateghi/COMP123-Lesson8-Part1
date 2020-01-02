d5779b58691701954240d1e70bad76320aba6bbc
knateghi <knateghi@my.centennialcollege.ca>
2020-01-01 22:37:02 -0500

    extracted DivideAge method

--- Program.cs
+++ Program.cs
@@ -10,7 +10,40 @@ namespace COMP123_Lesson8_Part1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your age: ");
+            int age,theNumber;
+            double result;
+
+            try
+            {
+                Console.Write("Enter your age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"You said your age is {age}");
+                result= DivideAge(age);
+                Console.WriteLine($"The result is {result} ");
+
+            }
+            catch (DivideByZeroException error)
+            {
+                Console.WriteLine("incorrect input=====> " + error.Message);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine();
+                Console.WriteLine("wrong input======> " + error.Message);
+            }
+            Console.WriteLine("finalltltt");
+            Console.ReadKey();
+        }
+
+        private static int DivideAge(int age)
+        {
+            int theNumber;
+            Console.Write("Enter a number to divide your age by: ");
+            theNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"the number you entered is {theNumber} ");
+            
+           
+            return (age / theNumber);
         }
     }
 }
